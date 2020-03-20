6019514f3ce96173c97ca1875ebc0f20bc900ea3
EvanGrinde <evan.grinde@gmail.com>
2020-03-20 13:51:25 -0400

    declare BoxList & ToolBar components in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoxListComponent } from './box-list/box-list.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxListComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule
